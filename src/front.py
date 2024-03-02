23107c040ec7e5f633ea743c1304b2fdeff77e3d
Vitor Miracca <vitormiracca@gmail.com>
2024-03-02 12:50:57 -0300

    feat: adicionando ingestão ao database e testes de integração

--- src/front.py
+++ src/front.py
@@ -20,3 +20,12 @@ class ValidadorExcelUI:
                 st.error(f"Erro de validação: {i}")
         else:
             st.success("O schema do arquivo Excel está correto!")
+
+    def display_save_button(self):
+        return st.button("Carregar os dados no Database")
+    
+    def display_wrong_message(self):
+        return st.error("Necessário corrigir a planilha!")
+    
+    def display_success_message(self):
+        return st.success("Dados salvos com sucesso no banco de dados!")
--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
+import logging
 from front import ValidadorExcelUI
-from back import processa_excel
+from back import excel_to_sql, processa_excel
 
 def main():
     ui = ValidadorExcelUI()
@@ -7,8 +8,17 @@ def main():
 
     upload_file = ui.upload_file()
     if upload_file:
-        result, errors = processa_excel(upload_file)
+        df, result, errors = processa_excel(upload_file)
         ui.display_results(result, errors)
+
+        if errors:
+            ui.display_wrong_message()
+            logging.error("Planilha apresentava erro no schema...")
+
+        elif ui.display_save_button():
+            excel_to_sql(df)
+            ui.display_success_message()
+            logging.info("Excel carregado com sucesso no banco SQL")
 
 
 if __name__ == "__main__":
